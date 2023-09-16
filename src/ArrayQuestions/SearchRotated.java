403b2e3567d6e6d72803b73a89ccbc4508df3cbf
Khalid <klood_12@hotmail.com>
2023-09-16 22:13:20 +0400

    Here i solved Search in Rotated Sorted Array qustion from leetcode using recursion

--- src/ArrayQuestions/SearchRotated.java
+++ src/ArrayQuestions/SearchRotated.java
@@ -2,28 +2,30 @@ package ArrayQuestions;
 
 public class SearchRotated {
 
-	public static void main(String[] args) {
-		int[] arr = {7,8,9,1,2,3,4};
-		int target = 7;
-		int ans = bin(arr, target, 0, arr.length - 1);
-		System.out.println(ans);
-	}
 	
-	static int bin(int[] arr, int target, int start, int end) {
-		if(start > end)
-			return -1;
-		int mid = start + (end - start) / 2;
-		if(arr[mid] == target)
-			return mid;
-		else if(arr[start] <= arr[mid]) {
-			if(target >= arr[start] && target <= arr[mid])
-				return bin(arr, target, start, mid - 1);
-			else
-				return bin(arr, target, mid + 1, end);
+	 // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+	 // 33. Search in Rotated Sorted Array
+	    int search(int[] arr, int target) {
+				int start = 0;
+	    	int end = arr.length - 1;
+	      return binarySearch(arr, target, start , end);
+	  	}
+
+	    int binarySearch(int[] arr, int target, int start, int end) {
+					if(start > end)
+						return -1;
+					int mid = start + (end - start) / 2;
+					if(arr[mid] == target)
+						return mid;
+					else if(arr[start] <= arr[mid]) {
+							if(target >= arr[start] && target <= arr[mid])
+								return binarySearch(arr, target, start, mid - 1);
+							else
+								return binarySearch(arr, target, mid + 1, end);
+						}
+					else if(target >= arr[mid] && target <= arr[end])
+						return binarySearch(arr, target, mid + 1, end);
+			
+					return binarySearch(arr, target, start, mid - 1); 
 		}
-		else if(target >= arr[mid] && target <= arr[end])
-			return bin(arr, target, mid + 1, end);
-		
-		return bin(arr, target, start, mid - 1); 
-	}
 }
