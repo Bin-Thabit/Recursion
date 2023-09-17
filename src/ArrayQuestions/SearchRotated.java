7ace16004e2ef9f1d8437b0feddfbc1043c676af
Khalid <klood_12@hotmail.com>
2023-09-17 14:54:09 +0400

    I modified SearchRotated.java and i solved Triangle1.java question using recursion

--- src/ArrayQuestions/SearchRotated.java
+++ src/ArrayQuestions/SearchRotated.java
@@ -5,27 +5,27 @@ public class SearchRotated {
 	
 	 // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 	 // 33. Search in Rotated Sorted Array
-	    int search(int[] arr, int target) {
-				int start = 0;
-	    	int end = arr.length - 1;
-	      return binarySearch(arr, target, start , end);
-	  	}
+	int search(int[] arr, int target) {
+		int start = 0;
+	    int end = arr.length - 1;
+	    return binarySearch(arr, target, start , end);
+	  }
 
-	    int binarySearch(int[] arr, int target, int start, int end) {
-					if(start > end)
-						return -1;
-					int mid = start + (end - start) / 2;
-					if(arr[mid] == target)
-						return mid;
-					else if(arr[start] <= arr[mid]) {
-							if(target >= arr[start] && target <= arr[mid])
-								return binarySearch(arr, target, start, mid - 1);
-							else
-								return binarySearch(arr, target, mid + 1, end);
+	int binarySearch(int[] arr, int target, int start, int end) {
+		if(start > end)
+			return -1;
+		int mid = start + (end - start) / 2;
+		if(arr[mid] == target)
+			return mid;
+		else if(arr[start] <= arr[mid]) {
+			if(target >= arr[start] && target <= arr[mid])
+				return binarySearch(arr, target, start, mid - 1);
+			else
+				return binarySearch(arr, target, mid + 1, end);
 						}
-					else if(target >= arr[mid] && target <= arr[end])
-						return binarySearch(arr, target, mid + 1, end);
+			else if(target >= arr[mid] && target <= arr[end])
+				return binarySearch(arr, target, mid + 1, end);
 			
-					return binarySearch(arr, target, start, mid - 1); 
-		}
+		return binarySearch(arr, target, start, mid - 1); 
+	}
 }
