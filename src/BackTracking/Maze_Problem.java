afab2ee3e84adf3dc57fa11b6eb56914e7997661
Khalid <klood_12@hotmail.com>
2023-09-28 15:26:09 +0400

    I add another way which return an arrayList that contains all possiible paths

--- src/BackTracking/Maze_Problem.java
+++ src/BackTracking/Maze_Problem.java
@@ -1,5 +1,7 @@
 package BackTracking;
 
+import java.util.ArrayList;
+
 public class Maze_Problem {
 
 	public static void main(String[] args) {
@@ -7,7 +9,10 @@ public class Maze_Problem {
 		// Print the count of paths
         System.out.println(countPath(1, 1));
         // Print the all possible paths
-        printPath("", 1, 1);
+        printPath1("", 1, 1);
+        System.out.println();
+        // Return ArrayList that contains all possible paths
+        System.out.println(printPath2("", 1, 1));
     }
 
 	static int countPath(int row , int col) {
@@ -21,17 +26,32 @@ public class Maze_Problem {
 		return left + right;
 	}
 
-    static void printPath(String p , int row , int col){
+    static void printPath1(String p , int row , int col){
         if(row == 3 && col == 3) {
             System.out.print(p + " ");
             return;
         }
 
         if(row < 3){
-        	printPath(p + "D" , row + 1 , col);
+        	printPath1(p + "D" , row + 1 , col);
         }
         if(col < 3)
-        	printPath(p + "R" , row , col + 1);
+        	printPath1(p + "R" , row , col + 1);
+    }
+    
+    static ArrayList<String> printPath2(String p , int row , int col){
+        if(row == 3 && col == 3) {
+        	ArrayList<String> list = new ArrayList();
+        	list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList();
+        if(row < 3)
+        	ans.addAll(printPath2(p + "D" , row + 1 , col));
+        
+        if(col < 3)
+        	ans.addAll(printPath2(p + "R" , row , col + 1));
+        return ans;
     }
     
 }
