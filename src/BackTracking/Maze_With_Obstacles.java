73bc43ccbae7eb1fb7ec336faa31c4fff1451cb6
Khalid <klood_12@hotmail.com>
2023-09-28 17:09:34 +0400

    I just modified the solution on Maze_With_Obstacles.class

--- src/BackTracking/Maze_With_Obstacles.java
+++ src/BackTracking/Maze_With_Obstacles.java
@@ -5,7 +5,7 @@ public class Maze_With_Obstacles {
 	public static void main(String[] args) {
         boolean[][] maze = {
                 {true,true,true},
-                {true,false,true},
+                {true,true,false},
                 {true,true,true}
         };
         Print_Paths_With_Obstacles("",0 , 0 , maze);
@@ -13,17 +13,17 @@ public class Maze_With_Obstacles {
     }
 
     static void Print_Paths_With_Obstacles(String p , int row , int col , boolean[][] maze){
-        if (row == 2 && col == 2){
+        if (row == maze.length - 1 && col == maze.length - 1){
             System.out.println(p);
             return;
         }
         if(maze[row][col] == false){
             return;
         }
-        if(row < 2) {
+        if(row < maze.length - 1) {
             Print_Paths_With_Obstacles(p + "D", row + 1, col, maze);
         }
-        if(col < 2) {
+        if(col < maze.length - 1) {
             Print_Paths_With_Obstacles(p + "R", row, col + 1, maze);
         }
     }
